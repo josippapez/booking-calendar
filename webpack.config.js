19a14d427bd30f5a90b42db42d4b68ba0b136c29
Josip Papež <papezjosip@gmail.com>
2022-07-13 11:46:00 +0200

    Feature/public-calendar
    Added env.example, also added fbConfig to fix build fails on vercel

--- webpack.config.js
+++ webpack.config.js
@@ -9,130 +9,151 @@ const {
 } = require('duplicate-dependencies-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const devMode = process.env.REACT_APP_NODE_ENV !== 'production';
 
 console.log('devMode', devMode);
 
-module.exports = {
-  context: path.join(__dirname, '/src'),
-  entry: {
-    app: { import: './index.tsx', dependOn: 'react-vendors' },
-    'react-vendors': [
-      'react',
-      'react-dom',
-      'react-router-dom',
-      'react-redux',
-      'redux',
-    ],
-  },
-  target: 'web',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/',
-    clean: true,
-  },
-  devServer: {
-    static: './public',
-    port: 3000,
-    liveReload: true,
-    hot: true,
-    historyApiFallback: {
-      index: '/',
+module.exports = () => {
+  return {
+    context: path.join(__dirname, '/src'),
+    entry: {
+      app: { import: './index.tsx', dependOn: 'react-vendors' },
+      'react-vendors': [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        'react-redux',
+        'redux',
+      ],
     },
-  },
-  watchOptions: {
-    ignored: ['**/node_modules', '**/dist', '**/.git'],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json', '.jsx'],
-    modules: ['node_modules', path.resolve(__dirname, 'src')],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
+    target: 'web',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].bundle.js',
+      publicPath: '/',
+      clean: true,
+    },
+    devServer: {
+      static: './public',
+      port: 3000,
+      liveReload: true,
+      hot: true,
+      historyApiFallback: {
+        index: '/',
       },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          devMode ? 'style-loader' : { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader', options: { importLoaders: 2 } },
-          'postcss-loader',
-        ],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          devMode ? 'style-loader' : { loader: MiniCssExtractPlugin.loader },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: true,
-              importLoaders: 2,
+    },
+    watchOptions: {
+      ignored: ['**/node_modules', '**/dist', '**/.git'],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json', '.jsx'],
+      modules: ['node_modules', path.resolve(__dirname, 'src')],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
+        {
+          test: /\.css$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            devMode ? 'style-loader' : { loader: MiniCssExtractPlugin.loader },
+            { loader: 'css-loader', options: { importLoaders: 2 } },
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            devMode ? 'style-loader' : { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: true,
+                importLoaders: 2,
+              },
+            },
+            { loader: 'postcss-loader' },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          exclude: /node_modules/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        minify: devMode
+          ? {}
+          : {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true,
+            },
+      }),
+      new DotenvWebpackPlugin({
+        path: './.env', // load this now instead of the ones in '.env'
+        safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+      }),
+      // new BundleAnalyzerPlugin({
+      //   analyzerMode: 'server',
+      //   openAnalyzer: false,
+      // }),
+      new DuplicateReporterPlugin(),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    optimization: {
+      minimize: !devMode,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
             },
           },
-          { loader: 'postcss-loader' },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        exclude: /node_modules/,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'server',
-    //   openAnalyzer: false,
-    // }),
-    new DuplicateReporterPlugin(),
-  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
-  optimization: {
-    minimize: !devMode,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          parse: {
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            inline: 2,
-          },
-          mangle: {
-            safari10: true,
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-          },
-        },
-        parallel: true,
-      }),
-    ],
-  },
-  devtool: 'source-map',
+          parallel: true,
+        }),
+      ],
+    },
+    devtool: 'source-map',
+  };
 };
