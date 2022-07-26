15b81c12ae0a98e0318b745526f6649dcc4beed2
Josip Papež <papezjosip@gmail.com>
2022-07-26 16:03:32 +0200

    fixes for serverless function execution

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,17 +1,15 @@
 import sgMail from "@sendgrid/mail";
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { NextApiRequest, NextApiResponse } from "next";
 const cors = require("cors")({ origin: true });
 
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
+module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
   cors(req, res, () => {
-    console.log(req.body);
-
-    if (req.body.data && req.body.data.msg && req.body.data.sendgrid_key) {
-      let sendgrid_key = req.body.data.sendgrid_key;
+    if (req.body && req.body.msg && req.body.sendgrid_key) {
+      let sendgrid_key = req.body.sendgrid_key;
       const template_id = "d-376c4c28bb694e09b22689b6ea3b977e";
       sgMail.setApiKey(sendgrid_key);
       sgMail
-        .send({ ...req.body.data.msg, templateId: template_id })
+        .send({ ...req.body.msg, templateId: template_id })
         .then(() => {
           return res.status(200).send("Email sent");
         })
