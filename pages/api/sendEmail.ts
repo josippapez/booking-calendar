e390333612f702c485bac7000c2b0d4353f5b59f
Josip Papež <papezjosip@gmail.com>
2022-07-26 15:54:22 +0200

    replacing firebase cloud functions with serverless nextjs function

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -4,17 +4,22 @@ const cors = require("cors")({ origin: true });
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   cors(req, res, () => {
-    let sendgrid_key = req.body.data.sendgrid_key;
-    const template_id = "d-376c4c28bb694e09b22689b6ea3b977e";
-    sgMail.setApiKey(sendgrid_key);
-    sgMail
-      .send({ ...req.body.data.msg, templateId: template_id })
-      .then(() => {
-        return res.status(200).send("Email sent");
-      })
-      .catch(error => {
-        console.error(error);
-        return res.status(500).send(error);
-      });
+    console.log(req.body);
+
+    if (req.body.data && req.body.data.msg && req.body.data.sendgrid_key) {
+      let sendgrid_key = req.body.data.sendgrid_key;
+      const template_id = "d-376c4c28bb694e09b22689b6ea3b977e";
+      sgMail.setApiKey(sendgrid_key);
+      sgMail
+        .send({ ...req.body.data.msg, templateId: template_id })
+        .then(() => {
+          return res.status(200).send("Email sent");
+        })
+        .catch(error => {
+          console.error(error);
+          return res.status(500).send(error);
+        });
+    }
+    return;
   });
 };
--- store/sendgridActions/emailActions.ts
+++ store/sendgridActions/emailActions.ts
@@ -1,12 +1,12 @@
-import firebase from 'firebase/compat/app';
-import { getFunctions, httpsCallable } from 'firebase/functions';
+import firebase from "firebase/compat/app";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import {
   fetchAndActivate,
   getRemoteConfig,
   getValue,
-} from 'firebase/remote-config';
-import { DateTime } from 'luxon';
-import { AppDispatch, AppState } from './../store';
+} from "firebase/remote-config";
+import { DateTime } from "luxon";
+import { AppDispatch, AppState } from "./../store";
 
 export const sendEmail = (
   reservation: {
@@ -21,28 +21,55 @@ export const sendEmail = (
   return async (dispatch: AppDispatch, getState: AppState) => {
     const remoteConfig = getRemoteConfig(firebase.app());
     await fetchAndActivate(remoteConfig);
-    const sendgrid_key = getValue(remoteConfig, 'sendgrid_key').asString();
+    const sendgrid_key = getValue(remoteConfig, "sendgrid_key").asString();
 
-    const functions = getFunctions(firebase.app(), 'europe-west3');
+    const functions = getFunctions(firebase.app(), "europe-west3");
 
-    const sendEmail = httpsCallable(functions, 'sendEmail');
-    await sendEmail({
-      msg: {
-        to: email,
-        from: 'booking.calendar.os.app@gmail.com',
-        subject: 'New reservation !!!!',
-        dynamic_template_data: {
-          start_date: DateTime.fromISO(reservation.start).toLocaleString(
-            DateTime.DATE_HUGE
-          ),
-          end_date: DateTime.fromISO(reservation.end).toLocaleString(
-            DateTime.DATE_HUGE
-          ),
-          reservation_name: reservation.title,
-          reservation_phone: reservation.phone,
-        },
+    // const sendEmail = httpsCallable(functions, 'sendEmail');
+    // await sendEmail({
+    //   msg: {
+    //     to: email,
+    //     from: 'booking.calendar.os.app@gmail.com',
+    //     subject: 'New reservation !!!!',
+    //     dynamic_template_data: {
+    //       start_date: DateTime.fromISO(reservation.start).toLocaleString(
+    //         DateTime.DATE_HUGE
+    //       ),
+    //       end_date: DateTime.fromISO(reservation.end).toLocaleString(
+    //         DateTime.DATE_HUGE
+    //       ),
+    //       reservation_name: reservation.title,
+    //       reservation_phone: reservation.phone,
+    //     },
+    //   },
+    //   sendgrid_key,
+    // });
+
+    fetch("api/sendEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      sendgrid_key,
+      body: JSON.stringify({
+        msg: {
+          to: email,
+          from: "booking.calendar.os.app@gmail.com",
+          subject: "New reservation !!!!",
+          dynamic_template_data: {
+            start_date: DateTime.fromISO(reservation.start).toLocaleString(
+              DateTime.DATE_HUGE
+            ),
+            end_date: DateTime.fromISO(reservation.end).toLocaleString(
+              DateTime.DATE_HUGE
+            ),
+            reservation_name: reservation.title,
+            reservation_phone: reservation.phone,
+          },
+        },
+        sendgrid_key,
+      }),
+    }).then(res => {
+      console.log(res);
     });
   };
 };
